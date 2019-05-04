441845d86990991cb0a4c3ccafe963a2352ba261
munhyunsu <munhyunsu@gmail.com>
2019-05-05 07:01:45 +0900

    config file

--- start_routine.py
+++ start_routine.py
@@ -11,6 +11,10 @@ if __name__ == '__main__':
     import argparse
     
     parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', type=str,
+                        default='config.ini',
+                        help=('configuration file. '
+                              'you can copy .template file'))
 
     FLAGS, _ = parser.parse_known_args()
     main(_)
